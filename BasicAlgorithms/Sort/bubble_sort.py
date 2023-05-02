d81e56394e4306e29332e6fdd23dcaecc0ea2012
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-05-03 01:37:02 +0900

    add comment

--- BasicAlgorithms/Sort/bubble_sort.py
+++ BasicAlgorithms/Sort/bubble_sort.py
@@ -1,7 +1,11 @@
 from typing import List
+import random
 
 
 def bubble_sort(numbers: List[int]) -> List[int]:
+    '''
+    隣の数字との大小を比較して、必要に応じて入れ替える
+    '''
     len_numbers = len(numbers)
     for i in range(len_numbers):
         for j in range(len_numbers - 1 - i):
@@ -11,6 +15,5 @@ def bubble_sort(numbers: List[int]) -> List[int]:
 
 
 if __name__ == '__main__':
-    import random
     nums = [random.randint(0, 1000) for _ in range(10)]
     print(bubble_sort(nums))
