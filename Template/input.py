5ed8dd1e325e3ee52c581e438f1471dc23c395c3
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-05-05 11:45:31 +0900

    create input.py

---
+++ Template/input.py
@@ -0,0 +1,11 @@
+# 一つの文字列の入力
+N = input()
+
+# 一つの数値の入力
+N = int(input)
+
+# 標準入力をint型で一行で受け取り、変数A, Bに代入する
+A, B = map(int, input().split())
+
+# 標準入力をint型で一括で受け取り、リストAに代入する
+A = list(map(int, input().split()))
